b8a6edc5ac93d56a3bbce512d769106ee1e47b90
wangjohn <wangjohn@mit.edu>
2013-01-08 00:12:16 -0500

    Added exception handling for when csv strings aren't the correct format.

--- parse_date.py
+++ parse_date.py
@@ -1,7 +1,10 @@
 import datetime
 
 def parse_to_datetime(created_at_string):
-    return datetime.datetime.strptime(created_at_string, "%Y-%m-%d %H:%M:%S.%f")
+    try:
+        return datetime.datetime.strptime(created_at_string, "%Y-%m-%d %H:%M:%S.%f")
+    except ValueError:
+        return datetime.datetime.strptime(created_at_string, "%Y-%m-%d %H:%M:%S")
 
 def parse_to_datetime_from_git(git_date_str):
     return datetime.datetime.strptime(git_date_str, "%a %b %d %H:%M:%S %Y +0000")
